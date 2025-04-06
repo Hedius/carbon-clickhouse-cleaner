d1c0a0c35dde49c45985d79aaad9a47adc327ba6
Hedius <git@hedius.eu>
2025-04-06 23:10:49 +0200

    prototype

--- carbon-clickhouse-cleaner.go
+++ carbon-clickhouse-cleaner.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"carbon-clickhouse-cleaner/config"
+	"carbon-clickhouse-cleaner/database"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const Version = "0.1.0"
 
 // 1. config reading
 // 2. setup logging
@@ -9,6 +20,117 @@ import "fmt"
 //  2. query tagged -> Delete tagged -> Return values
 //  3. trigger value delete.
 
+type Cleaner struct {
+	cfg   *config.Config
+	debug bool
+	ch    database.ClickHouse
+}
+
+func (c *Cleaner) Clean() error {
+	_, err := c.ch.Open()
+	if err != nil {
+		log.Error("Error opening ClickHouse connection: ", err)
+		return err
+	}
+	defer c.ch.Close()
+
+	now := time.Now()
+	timeStampPlain := now.Add(-time.Duration(c.cfg.Common.MaxAgePlain))
+	timeStampTagged := now.Add(-time.Duration(c.cfg.Common.MaxAgeTagged))
+
+	// 1. Get obsolete paths from index/tagged
+	obsoletePlainPaths, err := c.ch.GetPathsToDelete(c.ch.IndexTable, timeStampPlain)
+	if err != nil {
+		return err
+	}
+	obsoleteTaggedPaths, err := c.ch.GetPathsToDelete(c.ch.TaggedTable, timeStampTagged)
+	if err != nil {
+		return err
+	}
+
+	if len(obsoletePlainPaths) == 0 && len(obsoleteTaggedPaths) == 0 {
+		return nil
+	}
+
+	// 2. Trigger a DELETE for the points
+	err = c.ch.DeletePoints(timeStampPlain, timeStampTagged)
+	if err != nil {
+		return err
+	}
+
+	// 3. Delete the path with alter statements.
+	if len(obsoletePlainPaths) > 0 {
+		err = c.ch.DeletePaths(c.ch.IndexTable, timeStampPlain)
+		if err != nil {
+			return err
+		}
+	}
+	if len(obsoleteTaggedPaths) > 0 {
+		err = c.ch.DeletePaths(c.ch.TaggedTable, timeStampTagged)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var err error
+	configFile := flag.String("config", "carbon-clickhouse-cleaner.conf", "path to config file")
+	checkConfig := flag.Bool("check-config", false, "check config file and exit")
+	logLevel := flag.String("loglevel", "debug", "log level")
+	printVersion := flag.Bool("version", false, "print version and exit")
+	oneShot := flag.Bool("one-shot", false, "only run once if set")
+	debug := flag.Bool("debug", true, "enable debug mode (Do not run any delete)")
+
+	flag.Parse()
+
+	lvl, err := log.ParseLevel(*logLevel)
+	log.SetLevel(lvl)
+
+	if *printVersion {
+		fmt.Print(Version)
+		return
+	}
+
+	cfg, err := config.ReadConfig(*configFile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *checkConfig {
+		return
+	}
+
+	cleaner := &Cleaner{
+		cfg:   cfg,
+		debug: *debug,
+		ch: database.ClickHouse{
+			ConnectionString: cfg.ClickHouse.ConnectionString,
+			ValueTable:       cfg.ClickHouse.ValueTable,
+			IndexTable:       cfg.ClickHouse.IndexTable,
+			TaggedTable:      cfg.ClickHouse.TaggedTable,
+		},
+	}
+
+	for {
+		err = cleaner.Clean()
+		if err != nil {
+			log.Error("Error cleaning clickhouse: ", err)
+			if *oneShot {
+				os.Exit(1)
+			}
+		}
+
+		if *oneShot {
+			// abort if we one shot
+			break
+		}
+
+		log.Info("Sleeping for ", time.Duration(cfg.Common.LoopInterval))
+		time.Sleep(time.Duration(cfg.Common.LoopInterval))
+	}
+
 }
---
+++ database/clickhouse.go
@@ -0,0 +1,111 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/ClickHouse/clickhouse-go/v2"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+type ClickHouse struct {
+	ConnectionString string
+	ValueTable       string
+	IndexTable       string
+	TaggedTable      string
+
+	con *sql.DB
+}
+
+// Open gets a CH connection pool
+func (ch *ClickHouse) Open() (*sql.DB, error) {
+	db, err := sql.Open("clickhouse", ch.ConnectionString)
+	ch.con = db
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
+func (ch *ClickHouse) Close() {
+	if ch.con != nil {
+		err := ch.con.Close()
+		if err != nil {
+			log.Error("Error closing ClickHouse connection: ", err)
+		}
+		ch.con = nil
+	}
+}
+
+func (ch *ClickHouse) GetPathsToDelete(table string, maxTimeStamp time.Time) ([]string, error) {
+	query := fmt.Sprintf(
+		`SELECT DISTINCT Path, toDateTime(Max(Version)) AS MaxVersion
+				FROM %s
+				GROUP BY Path
+				HAVING MAX(Version) <= toUInt32(?)`, table)
+	rows, err := ch.con.Query(query, maxTimeStamp)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	defer rows.Close()
+	var paths []string
+	for rows.Next() {
+		var path string
+		var maxVersion time.Time
+		if err := rows.Scan(&path, &maxVersion); err != nil {
+			return nil, err
+		}
+		paths = append(paths, path)
+		log.Debugf("Path %v can be deleted. Last seen at %v", path, maxVersion)
+	}
+	log.Infof("%v paths/metrics can be deleted from %v", len(paths), table)
+	return paths, nil
+}
+
+func (ch *ClickHouse) DeletePoints(maxTimeStampPlain time.Time, maxTimeStampTagged time.Time) error {
+	var minTimeStamp time.Time
+	if maxTimeStampPlain.After(maxTimeStampTagged) {
+		minTimeStamp = maxTimeStampTagged
+	} else {
+		minTimeStamp = maxTimeStampTagged
+	}
+	query := fmt.Sprintf(
+		`DELETE FROM %s WHERE Date <= toDate(?) AND Path IN (
+					SELECT DISTINCT Path
+					FROM %s
+					GROUP BY Path
+					HAVING MAX(Version) <= toUInt32(?)
+				) OR Path IN (
+                	SELECT DISTINCT Path
+					FROM %s
+					GROUP BY Path
+					HAVING MAX(Version) <= toUInt32(?)
+				)`, ch.ValueTable, ch.IndexTable, ch.TaggedTable)
+	_, err := ch.con.Exec(query, minTimeStamp, maxTimeStampPlain, maxTimeStampTagged)
+	if err != nil {
+		log.Errorf("Error deleting points: %v", err)
+	}
+	return err
+}
+
+func (ch *ClickHouse) DeletePaths(table string, maxTimeStamp time.Time) error {
+	query := fmt.Sprintf(
+		`ALTER TABLE %s DELETE WHERE Date <= toDate(?)
+                AND Path IN (
+					SELECT DISTINCT Path
+					FROM %s
+					GROUP BY Path
+					HAVING MAX(Version) <= toUInt32(?)
+				)`, table, table)
+	_, err := ch.con.Exec(query, maxTimeStamp, maxTimeStamp)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	return err
+}
